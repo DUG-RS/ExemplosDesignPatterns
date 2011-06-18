81f0b1e67fce1420156d369d20c42d2938fad857
Daniel Wildt <dwildt@gmail.com>
2011-06-18 08:05:12 -0300

    dwildt: commit inicial

---
+++ src/ExemplosDesignPatterns.dpr
@@ -0,0 +1,14 @@
+program ExemplosDesignPatterns;
+
+uses
+  Forms,
+  UFrmMain in 'UFrmMain.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
---
+++ src/UFrmMain.pas
@@ -0,0 +1,24 @@
+unit UFrmMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TFrmMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmMain: TFrmMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
